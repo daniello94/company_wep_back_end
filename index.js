95c06106f4bd2b8bb15f0fb10f4dd59a60c672da
daniello94 <97047890+daniello94@users.noreply.github.com>
2024-03-12 19:33:56 +0100

    wersion wa

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const config = {
     origin: "http://" + process.env.DB_HOST
 };
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use('/uploads', express.static('uploads'));
 app.use("/gallery", galleryAdd)
 app.get("/", cors(config), function (req, res) {
